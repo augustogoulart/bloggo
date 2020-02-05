f72c9a64d48cd8b06234b556c5878a16c5e7a74f
Augusto <augusto@britecore.com>
2020-02-04 21:43:22 -0300

    Refactoring test suite

--- core/tests/test_read_time.py
+++ core/tests/test_read_time.py
@@ -5,17 +5,15 @@ from random import random, choice
 def test_calculate_read_time():
     one_minute_text = ['word'] * 265
     two_and_a_half_minutes_text = ['word'] * int(265 * 2.5)
+    ten_and_three_quarters_minutes_text = ['word'] * int(265 * 10.75)
 
     one_minute = read_time(one_minute_text)
-    two_minutes = read_time(one_minute_text * 2)
-    three_minutes = read_time(one_minute_text * 3)
-
     two_and_a_half_minutes_rounds_to_two_minutes = read_time(two_and_a_half_minutes_text)
+    eleven_minutes_text = read_time(ten_and_three_quarters_minutes_text)
 
     assert one_minute == 1
-    assert two_minutes == 2
-    assert three_minutes == 3
     assert two_and_a_half_minutes_rounds_to_two_minutes == 2
+    assert eleven_minutes_text == 11
 
 
 def test_calculate_generic_text_integer_read_time():
