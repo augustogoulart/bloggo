ae85633290ddf9c709712c79bd3fee6eaeb6ecf4
Augusto <augusto@britecore.com>
2020-02-05 21:52:01 -0300

    Add missing arguments to docstrings

--- core/read_time.py
+++ core/read_time.py
@@ -1,8 +1,11 @@
 def read_time(words):
     """
     Medium.com reading time formula: 265 words per minute
-    """
 
+    :param words: list of strings
+    :return: read time for a given text
+    :rtype: int
+    """
     number_of_words = len(words)
     words_per_minute = 265
 
