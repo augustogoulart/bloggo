eca1ef52eb3d66a00aa16f74b91e25d22a458a76
Augusto <augusto@britecore.com>
2020-05-14 22:36:50 -0300

    Add Linkedin

--- core/tests/test_social_links.py
+++ core/tests/test_social_links.py
@@ -7,7 +7,8 @@ def test_page_has_social_links(client):
         b'https://www.instagram.com/augusto_goulartc/',
         b'https://github.com/augustogoulart',
         b'https://twitter.com/gus_goulart',
-        b'https://t.me/augustogoulart'
+        b'https://t.me/augustogoulart',
+        b'https://www.linkedin.com/in/augustogoulart/'
     ]
 
     resp = client.get('/')
